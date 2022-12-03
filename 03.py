4adcc9e4b6bc7ddd02e25f83251e599756a2b7e3
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2022-12-03 20:07:44 +0200

    Further simplify day 3

--- 03.py
+++ 03.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 import numpy
-from string import ascii_lowercase as lowercase, ascii_uppercase as uppercase
+from string import ascii_letters as letters
 
 priorities = 0
 for line in open("03.txt"):
     index = len(line) // 2
     a, b = set(line[:index]), set(line[index:])
     common = a.intersection(b).pop()
-
-    if common in lowercase:
-        priorities += lowercase.index(common) + 1
-    elif common in uppercase:
-        priorities += uppercase.index(common) + len(uppercase) + 1
+    priorities += letters.index(common) + 1
 
 print(priorities)
 
@@ -21,10 +17,6 @@ groups = numpy.array_split(lines, len(lines) / 3)
 for group in groups:
     a, b, c = set(group[0]), set(group[1]), set(group[2])
     common = set.intersection(a, b, c).pop()
-
-    if common in lowercase:
-        priorities += lowercase.index(common) + 1
-    elif common in uppercase:
-        priorities += uppercase.index(common) + len(uppercase) + 1
+    priorities += letters.index(common) + 1
 
 print(priorities)
