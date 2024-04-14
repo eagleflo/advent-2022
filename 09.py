881787a4e80c5f7c2e7c80a35a373ab0dff58bb2
Aku Kotkavuo <aku.kotkavuo@sok.fi>
2024-04-14 18:22:08 +0300

    Leftovers from day 9

--- 09.py
+++ 09.py
@@ -1,44 +1,88 @@
 #!/usr/bin/env python3
 
-moves = open("09.txt").read().splitlines()
+moves = open("09_easy.txt").read().splitlines()
 
 head = (0, 0)
 tail = (0, 0)
 tail_history = [(0, 0)]
 
+
+class Knot:
+    def __init__(self) -> None:
+        self.x = 0
+        self.y = 0
+        self.history = []
+
+    def move(self, direction):
+        self.history.append((self.x, self.y))
+        match direction:
+            case "L":
+                self.x -= 1
+            case "R":
+                self.x += 1
+            case "U":
+                self.y -= 1
+            case "D":
+                self.y += 1
+
+    def touching(self, other):
+        return abs(self.x - other.x) < 2 and abs(self.y - other.y) < 2
+
+    def follow(self, direction, head):
+        match direction:
+            case "L":
+                if abs(self.x - head.x) == 2 and self.y == head.y:
+                    self.move(direction)
+                elif not self.touching(head):
+                    self.history.append((self.x, self.y))
+                    self.x -= 1
+                    self.y = head.y
+            case "R":
+                if abs(self.x - head.x) == 2 and self.y == head.y:
+                    self.move(direction)
+                elif not self.touching(head):
+                    self.history.append((self.x, self.y))
+                    self.x += 1
+                    self.y = head.y
+            case "U":
+                if abs(self.y - head.y) == 2 and self.x == head.x:
+                    self.move(direction)
+                elif not self.touching(head):
+                    self.history.append((self.x, self.y))
+                    self.y -= 1
+                    self.x = head.x
+            case "D":
+                if abs(self.y - head.y) == 2 and self.x == head.x:
+                    self.move(direction)
+                elif not self.touching(head):
+                    self.history.append((self.x, self.y))
+                    self.y += 1
+                    self.x = head.x
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Rope:
+    def __init__(self, length: int) -> None:
+        self.knots = []
+        for i in range(length):
+            self.knots.append(Knot())
+
+    def move(self, direction, amount):
+        for i in range(amount):
+            head, tail = self.knots[0], self.knots[1:]
+            head.move(direction)
+            for j, knot in enumerate(tail):
+                previous = self.knots[j]
+                knot.follow(direction, previous)
+
+
+rope_1 = Rope(2)
 for move in moves:
     direction, amount = move.split(" ")
     amount = int(amount)
-    for i in range(amount):
-        match direction:
-            case "L":
-                head = (head[0] - 1, head[1])
-                if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-                    tail = (head[0] + 1, tail[1])
-                    if tail[1] != head[1] and i > 0:
-                        tail = (tail[0], head[1])
-                tail_history.append(tail)
-            case "R":
-                head = (head[0] + 1, head[1])
-                if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-                    tail = (head[0] - 1, tail[1])
-                    if tail[1] != head[1] and i > 0:
-                        tail = (tail[0], head[1])
-                tail_history.append(tail)
-            case "U":
-                head = (head[0], head[1] - 1)
-                if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-                    tail = (tail[0], head[1] + 1)
-                    if tail[0] != head[0] and i > 0:
-                        tail = (head[0], tail[1])
-                tail_history.append(tail)
-            case "D":
-                head = (head[0], head[1] + 1)
-                if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-                    tail = (tail[0], head[1] - 1)
-                    if tail[0] != head[0] and i > 0:
-                        tail = (head[0], tail[1])
-                tail_history.append(tail)
+    rope_1.move(direction, amount)
 
-
-print(len(set(tail_history)))
+print(rope_1.knots[-1].history)
+print(len(set(rope_1.knots[-1].history)))
